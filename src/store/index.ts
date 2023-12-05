c890ce8e773b50c6bd443884d28f1b8252bc384f
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-06 01:31:08 +0300

    feat: add redux configuration

---
+++ src/store/index.ts
@@ -0,0 +1,41 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import userReducer from "./slices/userSlice"
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+interface IUser {
+  _id: string
+  email: string
+  birthday: string
+  phone: string
+  name: string
+}
+
+const initialState: IUser = {
+  _id: "",
+  email: "",
+  birthday: "",
+  phone: "",
+  name: "",
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (_, action: { payload: IUser; type: string }) => ({
+      ...action.payload,
+    }),
+    removeUser: () => ({
+      ...initialState,
+    }),
+  },
+})
+
+export const { setUser, removeUser } = userSlice.actions
+
+export default userSlice.reducer
