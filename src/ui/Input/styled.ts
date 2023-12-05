40ac4004f12acf3f05b25833ce63b47e3b428718
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-05 11:35:20 +0300

    fix: fix theme and remove magic number

--- src/ui/Input/styled.ts
+++ src/ui/Input/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const StyledInput = styled.input<{ $width?: string }>`
   width: ${({ $width }) => $width || "100%"};
   border: none;
-  padding: ${({ theme }) => theme.spacings.md};
+  padding: ${({ theme }) => theme.spacings.s15};
   border-radius: ${({ theme }) => theme.borderRadius};
   border: ${({ theme }) => theme.borderSize} solid ${({ theme }) => theme.lightGray};
 `
--- src/utils/formateBirthday.ts
+++ src/utils/formateBirthday.ts
@@ -1,6 +1,10 @@
 import { months } from "@/constants/dateConstants"
 
+const FIRST_TWO_DIGIT_NUMBER = 10
+
 export const formateBirthday = (year: number, month: string, day: number) => {
   const newMonth = months.indexOf(month) + 1
-  return `${day}.${newMonth < 10 ? `0${  newMonth}` : newMonth}.${year}`
+  return `${day < FIRST_TWO_DIGIT_NUMBER ? `0${day}` : day}.${
+    newMonth < FIRST_TWO_DIGIT_NUMBER ? `0${newMonth}` : newMonth
+  }.${year}`
 }
