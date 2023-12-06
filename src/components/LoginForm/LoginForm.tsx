20d5252b6329ad3c02825c1a983cf94c4c5821ae
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-06 21:16:18 +0300

    refactor: refactor login component

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,39 @@
+import { Fragment } from "react"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Error, LoginFormWrapper, SignUpLink, Title } from "./styled"
+import { Input } from "@/ui/Input/Input"
+import { Button } from "@/ui/Button/Button"
+import { Routes } from "@/constants/routes"
+import { ILoginFormProps } from "@/pages/Login/Login"
+import { loginSchema } from "@/validators/login"
+import { loginInputs } from "@/constants/loginFormParts"
+
+interface IFormProps {
+  onSubmit: (data: ILoginFormProps) => void
+  disabled: boolean
+}
+
+export function LoginForm({ disabled, onSubmit }: IFormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginFormProps>({ resolver: zodResolver(loginSchema) })
+  return (
+    <LoginFormWrapper onSubmit={handleSubmit(onSubmit)}>
+      <Title>Log in to Twitter</Title>
+      {loginInputs.map(({ placeholder, type, name }) => (
+        <Fragment key={placeholder}>
+          <Input {...register(name)} placeholder={placeholder} type={type} />
+          {errors[name] && <Error>{errors[name]?.message}</Error>}
+        </Fragment>
+      ))}
+
+      <Button type="submit" disabled={disabled} primary>
+        Log In
+      </Button>
+      <SignUpLink to={Routes.AUTH}>Sign up to Twitter</SignUpLink>
+    </LoginFormWrapper>
+  )
+}
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,36 +1,31 @@
-import { Fragment, useState } from "react"
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { useNavigate } from "react-router-dom"
-import { Error, LoginForm, SignUpLink, Title, Wrapper } from "./styled"
+import { Wrapper } from "./styled"
 import { Routes } from "@/constants/routes"
 import { loginSchema } from "@/validators/login"
 import { TwitterLogo } from "@/components/TwitterLogo/TwitterLogo"
-import { Input } from "@/ui/Input/Input"
-import { Button } from "@/ui/Button/Button"
-import { loginInputs } from "@/constants/loginFormParts"
+
 import { isValidEmail, isValidPhone } from "@/utils/validateIdentifier"
 import { db, signin } from "@/firebase"
 import { useAppDispatch } from "@/hooks/redux"
 import { notificationActions } from "@/store/slices/notificationSlice"
 import { NotificationStatuses } from "@/constants/notificationStatus"
+import { LoginForm } from "@/components/LoginForm/LoginForm"
 
-interface ILoginFormProps {
+export interface ILoginFormProps {
   identifier: string
   password: string
 }
 
 export function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<ILoginFormProps>({ resolver: zodResolver(loginSchema) })
+  const [disabled, setDisabled] = useState(false)
+  const { reset } = useForm<ILoginFormProps>({ resolver: zodResolver(loginSchema) })
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
-  const [disabled, setDisabled] = useState(false)
+
   const onSubmit = async (data: ILoginFormProps) => {
     try {
       setDisabled(true)
@@ -74,20 +69,7 @@ export function Login() {
   return (
     <Wrapper>
       <TwitterLogo />
-      <LoginForm onSubmit={handleSubmit(onSubmit)}>
-        <Title>Log in to Twitter</Title>
-        {loginInputs.map(({ placeholder, type, name }) => (
-          <Fragment key={placeholder}>
-            <Input {...register(name)} placeholder={placeholder} type={type} />
-            {errors[name] && <Error>{errors[name]?.message}</Error>}
-          </Fragment>
-        ))}
-
-        <Button type="submit" disabled={disabled} primary>
-          Log In
-        </Button>
-        <SignUpLink to={Routes.AUTH}>Sign up to Twitter</SignUpLink>
-      </LoginForm>
+      <LoginForm disabled={disabled} onSubmit={onSubmit} />
     </Wrapper>
   )
 }
