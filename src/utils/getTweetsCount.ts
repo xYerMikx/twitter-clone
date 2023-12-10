4cc1b7b2ccd2ddfb555a6767d7178505d5bc7d91
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-10 18:38:11 +0300

    feat: add tweets count
    fix conflicts

--- src/utils/getTweetsCount.ts
+++ src/utils/getTweetsCount.ts
@@ -5,9 +5,9 @@ export const getTweetCount = async (email: string) => {
   try {
     const tweetQuery = query(collection(db, "tweets"), where("email", "==", email))
     const querySnapshot = await getDocs(tweetQuery)
-
     return querySnapshot.size
   } catch (error) {
     console.error("Error fetching tweet count:", error)
   }
+  return undefined
 }
