1d045000f8a52b8d42b2a03f31ad57072a63ff61
Yan <101996018+xYerMikx@users.noreply.github.com>
2023-12-18 13:52:12 +0300

    feat: add EB and notfound page (#11)

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import { Routes } from "@/constants/routes"
+import { ErrorWrapper, StyledLink, StyledSubtitle, StyledTitle } from "./styled"
+
+export function NotFound() {
+  return (
+    <ErrorWrapper>
+      <StyledTitle>Something went wrong...</StyledTitle>
+      <StyledSubtitle>This page does not exist</StyledSubtitle>
+      <StyledLink to={Routes.HOME}>Go back to home page</StyledLink>
+    </ErrorWrapper>
+  )
+}
