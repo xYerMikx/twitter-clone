cb7538014bc6a3405842102addbddeb1f3d80796
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-24 21:17:15 +0300

    fix: fix default items on users and tweets

---
+++ src/utils/fetchItems.ts
@@ -0,0 +1,36 @@
+import { DocumentData, Query, getDocs } from "firebase/firestore"
+import { ISearchedTweet, IUserProfile } from "@/constants/mockData"
+
+export const fetchItems = async (
+  dataQuery: Query<DocumentData>,
+  isProfile: boolean,
+  profile?: string,
+  limit?: number,
+): Promise<ISearchedTweet[] | IUserProfile[]> => {
+  try {
+    const querySnapshot = await getDocs(dataQuery)
+    let newItems: ISearchedTweet[] | IUserProfile[] = []
+
+    if (isProfile) {
+      newItems = querySnapshot.docs.map((doc) => {
+        const data = doc.data() as IUserProfile
+        return { ...data, photoURL: profile } as IUserProfile
+      })
+    } else {
+      newItems = querySnapshot.docs.map((doc) => {
+        const { email, name, createdAt, content } = doc.data()
+        const { id } = doc
+        return { email, name, createdAt, content, id } as ISearchedTweet
+      })
+    }
+
+    if (limit) {
+      newItems = newItems.slice(0, limit)
+    }
+
+    return newItems
+  } catch (error) {
+    console.error("Error fetching data: ", error)
+    return []
+  }
+}
